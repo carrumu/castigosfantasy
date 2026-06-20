fe15d6ad24c40cdc16f2e25a2cbf66fd0eb3a878
nooehf <nhidfer2711@gmail.com>
2026-06-20 17:47:36 +0200

    arregloescapehtml

--- src/views/landing.js
+++ src/views/landing.js
@@ -433,3 +433,14 @@ export function renderLanding(container, callbacks) {
 
   loadData();
 }
+
+function escapeHTML(str) {
+  if (!str) return '';
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
